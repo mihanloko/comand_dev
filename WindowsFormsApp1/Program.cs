35ab5077065d974d16123df348d06c45b22ab1a8
kamotora <kamotora@yandex.ru>
2020-04-20 11:33:22 +0700

    Новая бд

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Komandirovki;
 
 namespace WindowsFormsApp1
 {
@@ -17,7 +13,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PersonalCard(228));
+            Application.Run(new MainWindow());
         }
     }
 }
